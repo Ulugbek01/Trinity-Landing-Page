3c93902068826662e17ea2c0921055d5104d5778
Ulugbek01 <xyz207820@gmail.com>
2022-09-26 09:50:25 +0500

    Media query add

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("main-navigation__items");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
